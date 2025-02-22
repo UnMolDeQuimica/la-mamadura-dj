3afe0e128cbf0a1e1ea6e4489a63e1ef7b85e5f4
Miguel Ángel Rodríguez Vidal <unmoldequimica@gmail.com>
2025-02-22 22:01:00 +0100

    Initial commit

---
+++ la_mamadura/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from la_mamadura.users.models import User
+from la_mamadura.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
